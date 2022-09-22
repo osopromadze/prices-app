741e98471b493a1a293196f3c9b381863efe7c41
Omari Sopromadze <omari.sopromadze@kairosds.com>
2022-09-22 23:04:05 +0200

    refactor: move getting price logic in domain layer

--- src/main/java/com/kairosds/pricesapp/application/usecases/GetPriceForDateUseCase.java
+++ src/main/java/com/kairosds/pricesapp/application/usecases/GetPriceForDateUseCase.java
@@ -4,14 +4,10 @@ import com.kairosds.pricesapp.application.dto.PriceForDateInput;
 import com.kairosds.pricesapp.application.dto.PriceForDateOutput;
 import com.kairosds.pricesapp.application.mapper.PriceMapper;
 import com.kairosds.pricesapp.domain.exception.BrandNotFoundException;
-import com.kairosds.pricesapp.domain.exception.PriceNotFoundException;
 import com.kairosds.pricesapp.domain.model.Price;
 import com.kairosds.pricesapp.domain.service.BrandService;
 import com.kairosds.pricesapp.domain.service.PriceService;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class GetPriceForDateUseCase {
@@ -26,24 +22,8 @@ public class GetPriceForDateUseCase {
             throw new BrandNotFoundException(String.format("Brand with id %d not found", input.getBrandId()));
         }
 
-        List<Price> prices = priceService.getPrices().stream()
-                .filter(price -> price.getBrandId().equals(input.getBrandId()))
-                .filter(price -> price.getProductId().equals(input.getProductId()))
-                .filter(price -> input.getDate().isAfter(price.getStartDate()) && input.getDate().isBefore(price.getEndDate()))
-                .collect(Collectors.toList());
+        Price price = priceService.getPrice(input.getDate(), input.getProductId(), input.getBrandId());
 
-        Long highestPriority = prices.stream()
-                .mapToLong(price -> price.getPriority())
-                .max()
-                .orElse(-1);
-
-        Price price = prices.stream()
-                .filter(p -> highestPriority.equals(p.getPriority()))
-                .findFirst()
-                .orElseThrow(() -> new PriceNotFoundException(String.format("Price for product with id %d not found", input.getProductId())));
-
-        PriceMapper priceMapper = new PriceMapper();
-
-        return priceMapper.toOutput(price, input.getDate());
+        return new PriceMapper().toOutput(price, input.getDate());
     }
 }
--- src/main/java/com/kairosds/pricesapp/domain/service/PriceService.java
+++ src/main/java/com/kairosds/pricesapp/domain/service/PriceService.java
@@ -1,17 +1,34 @@
 package com.kairosds.pricesapp.domain.service;
 
+import com.kairosds.pricesapp.domain.exception.PriceNotFoundException;
 import com.kairosds.pricesapp.domain.model.Price;
 import com.kairosds.pricesapp.domain.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class PriceService {
 
     private final PriceRepository priceRepository;
 
-    public List<Price> getPrices() {
-        return priceRepository.getPrices();
+    public Price getPrice(LocalDateTime date, Long productId, Long brandId) {
+        List<Price> prices = priceRepository.getPrices().stream()
+                .filter(price -> price.getBrandId().equals(brandId))
+                .filter(price -> price.getProductId().equals(productId))
+                .filter(price -> date.isAfter(price.getStartDate()) && date.isBefore(price.getEndDate()))
+                .collect(Collectors.toList());
+
+        Long highestPriority = prices.stream()
+                .mapToLong(price -> price.getPriority())
+                .max()
+                .orElse(-1);
+
+        return prices.stream()
+                .filter(p -> highestPriority.equals(p.getPriority()))
+                .findFirst()
+                .orElseThrow(() -> new PriceNotFoundException(String.format("Price for product with id %d not found", productId)));
     }
 }
